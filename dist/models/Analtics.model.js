0502e3eae35288ceab8ba319d41afda4bfd6bcfd
pmuhire2002@gmail.com <pmuhire2002@gmail.com>
2024-02-24 14:31:34 +0200

    Fix: cors

--- dist/models/Analtics.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mapAnalyticsToPrismaModel = void 0;
-const mapAnalyticsToPrismaModel = (analytics) => (Object.assign({}, analytics));
-exports.mapAnalyticsToPrismaModel = mapAnalyticsToPrismaModel;
-//# sourceMappingURL=Analtics.model.js.map
